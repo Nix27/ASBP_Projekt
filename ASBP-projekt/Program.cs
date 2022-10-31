e22669ca75ba2e397a69fbfb945f438374eb229d
Nix27 <nikolazecic2468@gmail.com>
2022-10-31 21:31:29 +0100

    80% gotovo, još dizajn

---
+++ ASBP-projekt/Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ASBP_projekt.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ASBP_projektContextConnection") ?? throw new InvalidOperationException("Connection string 'ASBP_projektContextConnection' not found.");
+
+builder.Services.AddDbContext<ASBP_projektContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ASBP_projektContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+//builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme).AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration);
+builder.Services.AddMvc(option =>
+{
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    option.Filters.Add(new AuthorizeFilter(policy));
+}).AddMicrosoftIdentityUI();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapRazorPages();
+});
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ ASBP-projekt/wwwroot/js/currencyConverter.js
@@ -0,0 +1,45 @@
+﻿const currencies = {
+    AUD: "Australian Dollar",
+    CAD: "Canadian Dollar",
+    EUR: "Euro",
+    GBP: "British Pound",
+    INR: "Indian Rupee",
+    JPY: "Japanese Yen",
+    USD: "United States Dollar",
+    ZAR: "South African Rand",
+};
+
+function getOptions(data) {
+    return Object.entries(data).map(([country, currency]) => `<option value="${country}">${country} - ${currency}</option>`).join("");
+}
+
+const fromCurrency = document.getElementById("currenciesFrom");
+const toCurrency = document.getElementById("currenciesTo");
+fromCurrency.innerHTML = getOptions(currencies);
+toCurrency.innerHTML = getOptions(currencies);
+
+document.getElementById("convert").addEventListener("click", fetchCurrencies);
+
+function fetchCurrencies() {
+    const from = fromCurrency.value;
+    const to = toCurrency.value;
+    const enteredAmount = document.getElementById("amount").value;
+
+    fetch("https://v6.exchangerate-api.com/v6/866b3f10ca915189caf84a89/latest/" + from).then((response) => {
+        if (response.ok) {
+            return response.json();
+        }
+        else {
+            throw new Error("Network error response");
+        }
+    }).then((data) => {
+        console.log(data);
+        displayCurrency(data, from, to, enteredAmount);
+    }).catch((error) => console.error("Fetch error: ", error));
+}
+
+function displayCurrency(data, from, to, enteredAmount) {
+    const converted = enteredAmount * data.conversion_rates[to];
+    document.getElementById("result").value = converted + " " + to;
+    console.log(converted + " " + to);
+}
